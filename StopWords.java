762b3e0b5915c9c993af5bff169bcf402ee7cdb9
Felipe Vilhena Dias <121449886+felipevidias@users.noreply.github.com>
2024-11-21 12:42:37 -0300

    Update StopWords.java

--- StopWords.java
+++ StopWords.java
@@ -49,7 +49,7 @@ public class StopWords {
   public StopWords() {
     File arquivo;
     try {
-      arquivo = new File("stopWordsList.txt");
+      arquivo = new File("StopWords.txt");
       Scanner scanner = new Scanner(arquivo);
       while (scanner.hasNext()) {
         String linha = scanner.nextLine();
